1114156b4d09af35a5e9366631934061fe5b431d
ScarZeus <kevingladson2427@gmail.com>
2026-03-01 19:43:45 +0530

    Initial Structure

---
+++ backend/apps/attendace/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class Attendance(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateField()
+    check_in = models.TimeField(null=True, blank=True)
+    check_out = models.TimeField(null=True, blank=True)
+    status = models.CharField(max_length=20, default="Present")
+
+    class Meta:
+        unique_together = ('user', 'date')
